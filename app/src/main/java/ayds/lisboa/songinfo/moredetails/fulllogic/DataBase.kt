ea8d29157c364f53f694386e0bdcfb2938df76d4
guadalupecarreno <guadalupe-c@hotmail.com>
2022-04-27 10:29:59 -0300

    Corecciones de constantes en database

--- app/src/main/java/ayds/lisboa/songinfo/moredetails/fulllogic/DataBase.kt
+++ app/src/main/java/ayds/lisboa/songinfo/moredetails/fulllogic/DataBase.kt
@@ -7,25 +7,33 @@ import android.content.Context
 import android.database.Cursor
 import android.util.Log
 
-class DataBase(context: Context?) : SQLiteOpenHelper(context, "dictionary.db", null, 1) {
+const val DATABASE_NAME = "dictionary.db"
+const val ARTIST_COLUMN = "artist"
+const val INFO_COLUMN = "info"
+const val SOURCE_COLUMN = "source"
+const val ID_COLUMN = "id"
+const val ARTIST_TABLE_NAME = "artists"
+const val ARTIST_TABLE_QUERY = "create table $ARTIST_TABLE_NAME ($ID_COLUMN INTEGER PRIMARY KEY AUTOINCREMENT, $ARTIST_COLUMN string, $INFO_COLUMN string, $SOURCE_COLUMN integer)"
+
+class DataBase(context: Context?) : SQLiteOpenHelper(context, DATABASE_NAME, null, 1) {
 
    override fun onCreate(db: SQLiteDatabase) {
         db.execSQL(
-            "create table artists (id INTEGER PRIMARY KEY AUTOINCREMENT, artist string, info string, source integer)"
+            ARTIST_TABLE_QUERY
         )
     }
 
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {}
 
     fun saveArtist(artist: String?, info: String?) {
-        this.writableDatabase.insert("artists", null, getContentValues(artist,info))
+        this.writableDatabase.insert(ARTIST_TABLE_NAME, null, getContentValues(artist,info))
     }
 
     private fun getContentValues(artist: String?,info: String?): ContentValues {
         val values = ContentValues()
-        values.put("artist", artist)
-        values.put("info", info)
-        values.put("source", 1)
+        values.put(ARTIST_COLUMN, artist)
+        values.put(INFO_COLUMN, info)
+        values.put(SOURCE_COLUMN, 1)
         return values
     }
 
@@ -33,21 +41,20 @@ class DataBase(context: Context?) : SQLiteOpenHelper(context, "dictionary.db", n
 
     private fun makeQuery(artist: String): Cursor =
         this.readableDatabase.query(
-            "artists",
-            arrayOf("id", "artist", "info"),
-            "artist  = ?",
+            ARTIST_TABLE_NAME,
+            arrayOf(ID_COLUMN, ARTIST_COLUMN, INFO_COLUMN),
+            "$ARTIST_COLUMN  = ?",
             arrayOf(artist),  
             null,
             null,
-            "artist DESC"
+            "$ARTIST_COLUMN DESC"
         )
-
 
     private fun getArtistInfoFromQuery(cursor: Cursor) : String?{
         var artistInfo: String? = null
         if (cursor.moveToNext()) {
             artistInfo = cursor.getString(
-                cursor.getColumnIndexOrThrow("info")
+                cursor.getColumnIndexOrThrow(INFO_COLUMN)
             )
         }
         cursor.close()

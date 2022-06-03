dc402dcb894fc34f13c522dad3e79bf97b6131ff
brancomartinezc <bmartinezcarpineti@gmail.com>
2022-06-03 16:15:13 -0300

    arreglando conexion submodulo

--- libs/lastfm/src/main/java/ayds/lisboa2/lastFM/LastFMArtist.kt
+++
@@ -1,9 +0,0 @@
-package ayds.lisboa2.lastFM
-
-const val LASTFM_LOGO = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Lastfm_logo.svg/320px-Lastfm_logo.svg.png"
-
-data class LastFMArtist (
-    val artistName: String,
-    val description: String,
-    val infoURL: String,
-)

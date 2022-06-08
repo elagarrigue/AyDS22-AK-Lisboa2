f2c03471f3cc6e4162184be254ec916aeed61a99
brancomartinezc <bmartinezcarpineti@gmail.com>
2022-06-08 11:40:49 -0300

    fix cardFormatterTester

--- app/src/test/java/ayds/lisboa/songinfo/moredetails/view/CardFormatterTester.kt
+++ app/src/test/java/ayds/lisboa/songinfo/moredetails/view/CardFormatterTester.kt
@@ -1,28 +1,18 @@
 package ayds.lisboa.songinfo.moredetails.view
 
-import ayds.lisboa.songinfo.moredetails.model.Source
-import ayds.lisboa.songinfo.moredetails.model.entities.Card
-import ayds.lisboa.songinfo.moredetails.model.entities.CardImpl
-import ayds.lisboa.songinfo.moredetails.model.entities.EmptyCard
 import org.junit.Assert
 import org.junit.Test
 
 class CardFormatterTester {
 
     private val cardFormatter = CardFormatterImpl()
+    private val name = "artistName"
+    private val description = "this is the artist description"
 
     @Test
     fun `given a local artist it should return the description`() {
-        val card: Card = CardImpl(
-            "artistName",
-            "this is the artist description",
-            "url",
-            Source.LASTFM,
-            "logoUrl",
-            true
-        )
 
-        val result = cardFormatter.getStringCardInfo(card)
+        val result = cardFormatter.getStringCardInfo(description, name, true)
 
         val expected =
             "[*]<html><div width=400><font face=\"arial\">this is the artist description</font></div></html>"
@@ -32,16 +22,8 @@ class CardFormatterTester {
 
     @Test
     fun `given a non local artist it should return the description`() {
-        val card: Card = CardImpl(
-            "artistName",
-            "this is the artist description",
-            "url",
-            Source.LASTFM,
-            "logoUrl",
-            false
-        )
 
-        val result = cardFormatter.getStringCardInfo(card)
+        val result = cardFormatter.getStringCardInfo(description, name, false)
 
         val expected =
             "<html><div width=400><font face=\"arial\">this is the artist description</font></div></html>"
@@ -49,14 +31,4 @@ class CardFormatterTester {
         Assert.assertEquals(expected, result)
     }
 
-    @Test
-    fun `given a non existing artist it should return the artist not found description`() {
-        val card: Card = EmptyCard
-
-        val result = cardFormatter.getStringCardInfo(card)
-
-        val expected = "<html><div width=400><font face=\"arial\"></font></div></html>"
-
-        Assert.assertEquals(expected, result)
-    }
 }
